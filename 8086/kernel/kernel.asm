d0992b7801fe14bd7d6bb3be42abf3e609d48946
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-04-18 16:50:10 +0530

    A tiny little subroutine to show how values of CS, SS, BP, and SP have been changed

--- 8086/kernel/kernel.asm
+++ 8086/kernel/kernel.asm
@@ -1,24 +1,74 @@
 bits 16
 section .text
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; KERNEL ENTRY POINT
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; MEMORY MAP
+; 0x9A000:| BIOS related stuff |
+;          --------------------
+; 0x99fff:| Stack Base Pointer |
+; 	  |	   ...	       |
+;   ...   ~		       ~
+;         |        ...         |
+; 0x90000:| Stack Segment (SS) |
+; 	   --------------------
+; 	  |	   ...	       |
+;   ...   ~		       ~
+;         |        ...         |
+; 0x20000:|        ...         |
+; 0x10000:|    16-bit Kernel   |
+; 	   --------------------
+; 	  |	   ...	       |
+;   ...   ~		       ~
+; 0x7e00  |        ...         |
+;	   --------------------
+; 0x7cff: |        ...         |
+; 0x7c00: | 16-bit Bootloader  |
+;          --------------------
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 kernel_entry:
 	mov ax, cs
 	mov ds, ax
 	mov es, ax
+	mov ax, 0x9000
+	mov ss, ax
+	mov bp, 0x9fff
+	mov sp, bp
 
 kernel_main:
-	mov si, MSG1
-	call print
-	call printnl
-	mov dx, 0x1af4
-	call printhex
 	call printnl
 	mov si, MSG1
 	call print
-	mov dx, 0xff12
+	call printnl
+	
+	mov si, word [MSGCS]
+	call print
+	mov dx, cs
 	call printhex
 	call printnl
+	
+	mov si, word [MSGSS]
+	call print
+	mov dx, ss
+	call printhex
+	call printnl
+
+	mov si, word [MSGBP]
+	call print
+	mov dx, bp
+	call printhex
+	call printnl
+	
+	call func
 	jmp $
+func: 
+	mov si, word [MSGSP]
+	call print
+	mov dx, sp
+	call printhex
+	call printnl
+	ret
 
 print:
 	mov bx, si
@@ -73,4 +123,8 @@ printnl:
 	ret	
 
 section .data
-	MSG1 db "Xello world", 0x00
+	MSG1 db "Hello world", 0x00
+	MSGCS db "CS = ", 0x00
+	MSGSS db "SS = ", 0x00
+	MSGBP db "BP = ", 0x00
+	MSGSP db "SP = ", 0x00
