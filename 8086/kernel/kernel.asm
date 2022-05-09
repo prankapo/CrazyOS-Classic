2fc0e220a43661cdd1d91b9b9c4513f26613c783
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-05-09 18:14:08 +0530

    EXAMPLE: How to show time
    Calling works as long as you are in the same file :/

--- 8086/kernel/kernel.asm
+++ 8086/kernel/kernel.asm
@@ -27,6 +27,9 @@ align 16
 ; 0x7c00: | 16-bit Bootloader  |
 ;          --------------------
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+%define RTCaddress 0x70
+%define RTCdata 0x71
 kernel_entry:
 	mov ax, cs
 	mov ds, ax
@@ -40,6 +43,7 @@ kernel_main:
 	call clear
 	mov si, TEST_MSG
 	call printf
+	call showtime
 .while:
 	call getline
 	mov si, GETLINE_BUFFER
@@ -49,5 +53,26 @@ kernel_main:
 	jmp .while
 
 	TEST_MSG: dw "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\n", 0x00
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; SHOWTIME
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+showtime:
+.1:	
+	mov al, 0x0a
+	out RTCaddress, al
+	in al, RTCdata
+	test al, 0x80		; if bit 7 is set, then an update is in progress
+	je .1
+	
+	mov al, 0x04
+	out RTCaddress, al
+	in al, RTCdata
+	call print8bitpackedBCD
+	mov al, 0x02
+	out RTCaddress, al
+	in al, RTCdata
+	call print8bitpackedBCD
+	ret
 
 %include "include/ttyio/ttyio.asm"
