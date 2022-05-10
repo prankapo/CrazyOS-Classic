c6a22e9a3613ef672321add00bccc96623a5c7c1
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-05-10 09:04:01 +0530

    Lexer checkpoint # 2

--- 8086/shell/shell.asm
+++ 8086/shell/shell.asm
@@ -8,47 +8,60 @@ main:
         call getline
         mov si, GETLINE_BUFFER
         ; do processing here
-        mov byte [flag], 0x00
-        mov cx, 0x00
-        cld
-lexer:
-        cmp si, GETLINE_BUFFER_END
-        je .exit
-        cmp byte [si], 0x00
-        je .exit
-        lodsb
-        cmp al, ' '
-        jne .store
-        mov byte [flag], 0x00
-        jmp lexer
-.store:
-        cmp byte [flag], 0x00
-        je .setflag
-.store1:
-        mov byte [di], al
-        inc di
-        jmp lexer
-.setflag:
-        inc byte [flag]
-        push ax
-        mov ax, 0x10
-        mul cx
-        mov di, com
-        add di, ax
-        inc cx
-        pop ax
-        jmp .store1
-.exit:
-        mov si, com
-        call print
-        call printnl
-	
-        mov al, 's'
-        call putchar
-        call printnl
+        call lexer
         call flush                      ; flush the line
         jmp main
 
+
+
+
+lexer:
+        mov byte [flag], 0x00
+        mov cx, 80d
+        mov di, com
+.comargflush:
+        mov al, 'X'
+        call putchar
+        mov byte [di], 0x00
+        inc di
+        loop .comargflush
+        mov si, GETLINE_BUFFER
+        mov di, com
+        mov cx, 80
+        mov dx, 0x00
+.storewords:
+        mov al, byte [si]
+        push ax
+        call putchar
+        pop ax
+        inc si
+        cmp al, 0x00
+        je .lexit
+        cmp al, ' '
+        je .clearflag
+        cmp byte [flag], 0x00
+        je .setflag
+        jne .store
+.clearflag:
+        mov byte [flag], 0x00
+        loop .storewords
+.setflag:
+        inc byte [flag]
+        push ax
+        mov ax, 16
+        mul dl
+        add di, ax
+        inc dx
+        pop ax
+.store:
+        mov byte [di], al
+        inc di
+        loop .storewords
+.lexit:
+        mov si, com
+        call print
+        call printnl
+        ret
         flag: db 0x00 
         com: times 16 db 0x00
         arg1: times 16 db 0x00
