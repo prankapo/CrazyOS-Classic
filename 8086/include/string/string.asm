2aba7035f52b304024b5bb0ffb8552b4771dbc46
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-05-11 23:27:47 +0530

    strcmp checkpoint #3

--- 8086/include/string/string.asm
+++ 8086/include/string/string.asm
@@ -12,10 +12,6 @@ strlen:
         sub di, si
         dec di
         xchg ax, di
-        push ax
-        call printdec
-        call printnl
-        pop ax
         ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -24,26 +20,32 @@ strlen:
 strcmp:
         mov word [.ptr1], si
         mov word [.ptr2], di
-        push si
-        call print
-        pop si
+        call printnl
         call strlen
-        mov word [.n1], ax
-        mov si, word [.ptr2]
-        call strlen
-        mov word [.n2], ax
-        cmp ax, word [.n1]
-        jne .false
         mov cx, ax
         mov si, word [.ptr1]
         mov di, word [.ptr2]
-        repe cmpsb
+.1:
+        mov al, byte [si]
+        cmp byte [di], al
+        jne .false
+        inc di
+        inc si
+        loop .1
+        cmp byte [di], 0x00
+        jne .false
+        mov al, 'T'
+        call putchar
         cmp cx, 0x00
         je .true
 .false:
+        mov al, 'F'
+        call putchar
         mov ax, 0x01
         ret
 .true:
+        mov al, 'T'
+        call putchar
         mov ax, 0x00
         ret
         .ptr1: dw 0x00
