7835a51d0c47a7cfb5008958e7885d40e5573d8e
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-05-12 20:04:30 +0530

    atoi() and atoh() have been implemented

--- 8086/include/string/README.txt
+++ 8086/include/string/README.txt
@@ -7,7 +7,40 @@ OUTPUT: AX = length of the string in bytes
 
 FUNCTION NAME: strcmp
 DESCRIPTION: Compares two strings
-INPUT: SI = Pointer to string number 1
-        DI = Pointer to string number 2
+INPUT: SI = Pointer to string 1
+        DI = Pointer to string 2
 OUTPUT: If AX = 0, then both the strings are equal
         Else if AX = 1, then the strings are not equal
+
+FUNCTION NAME: atoi
+DESCRIPTION: Converts an integer string to an integer
+INPUT: SI = Pointer to string
+OUTPUT: AX = integer
+EXAMPLES: 
+        lea si, [NUM]
+        call atoi               ; will store the integer present in the string in AX
+        call printdec
+        call printnl
+             ...
+        NUM: db "1234", 0x00
+NOTE: 1.) Integer strings with more than 5 digits are not supported. An error will 
+          be thrown if more than 5 digits are there in the integer string.
+      2.) Negative integers are not supported.
+
+FUNCTION NAME: atoh
+DESCRIPTION: Converts a string storing a hexadecimal number to a hexadecimal number
+INPUT: SI = Pointer to string
+OUTPUT: AX = Hexadecimal number
+EXAMPLES: 
+        lea si, [NUM]
+        call atoh               ; will store the hex-integer in the string in AX
+        call printhex
+        call printnl
+             ...
+        NUM: db "1A3F", 0x00
+NOTE: 1.) Although the refix "0x" is used when printing hexadecimal numbers, make 
+          sure that the string being passed to atoh does not have this prefix.
+      2.) Only use uppercase alphabetical hex-digits i.e., A-F, and not a-f.
+      3.) Hexadecimal numeric strings with more than 4 digits will throw an error.
+      4.) Negativee integers are not supported.
+        
--- 8086/include/string/string.asm
+++ 8086/include/string/string.asm
@@ -45,4 +45,106 @@ strcmp:
         ret
         .ptr1: dw 0x00
         .ptr2: dw 0x00
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; ATOI
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+atoi:
+        push si
+        call strlen
+        cmp ax, 0x05
+        jg .error_length_exceeded
+        mov cx, 0x05
+        sub cx, ax     
+        mov bx, .t4
+.starting_multiplier:
+        add bx, 0x02
+        loop .starting_multiplier
+        mov cx, ax
+.1:     
+        lodsb
+        sub al, '0'
+        xor ah, ah
+        xor dx, dx
+        mul word [bx]
+        add word [.sum], ax
+        add bx, 0x02
+        loop .1
+        jmp .return_point
+
+.error_length_exceeded:
+        mov si, .ERR_LENGTH_EXCEEDED
+        call printf
+        jmp .return_point
+
+.return_point:
+        mov ax, word [.sum]
+        mov word [.sum], 0x00
+        pop si
+        ret
+        .t4: dw 10000
+        .t3: dw 1000
+        .t2: dw 100
+        .t1: dw 10
+        .t0: dw 1
+        .sum: dw 0
+        .ERR_LENGTH_EXCEEDED: dw "Error: More than 5 decimal digits not supported\n", 0x00
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; ATOH
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+atoh:
+        push si
+        call strlen
+        cmp ax, 0x04
+        jg .error_length_exceeded
+        mov cx, 0x04
+        sub cx, ax
+        mov bx, .t3
+.starting_multiplier:
+        add bx, 0x02
+        loop .starting_multiplier
+        mov cx, ax
+.1:
+        lodsb
+        cmp al, '9'
+        jle .less_than_9
+        cmp al, 'F'
+        jle .less_than_F
+        jmp .error_invalid_digit
+.2:
+        xor ah, ah
+        xor dx, dx
+        mul word [bx]
+        add word [.sum], ax
+        add bx, 0x02
+        loop .1
+        jmp .return_point
+.less_than_9:
+        sub al, '0'
+        jmp .2
+.less_than_F:
+        sub al, 'A'
+        add al, 0x0a
+        jmp .2
+.error_length_exceeded:
+        mov si, .ERR_LENGTH_EXCEEDED
+        call printf
+        jmp .return_point
+.error_invalid_digit:
+        mov si, .ERR_INVALID_DIGIT
+        call printf
+        jmp .return_point
+.return_point:
+        mov ax, word [.sum]
+        mov word [.sum], 0x00
+        pop si
+        ret
+        .t3: dw 4096
+        .t2: dw 256
+        .t1: dw 16
+        .t0: dw 1
+        .sum: dw 0x00
+        .ERR_LENGTH_EXCEEDED: dw "Error: More than 4 hex-digits not supported\n", 0x00
+        .ERR_INVALID_DIGIT: dw "Error: Invalid hex-digit\n", 0x00
 %endif
